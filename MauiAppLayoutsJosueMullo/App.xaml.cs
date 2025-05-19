0f9dcc22bca2a649264d6e66fbf2c48c59300527
Josue Mullo <josue.mullo@udla.edu.ec>
2025-05-19 08:41:34 -0500

    Add project files.

---
+++ MauiAppLayoutsJosueMullo/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiAppLayoutsJosueMullo
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ MauiAppLayoutsJosueMullo/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiAppLayoutsJosueMullo
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
