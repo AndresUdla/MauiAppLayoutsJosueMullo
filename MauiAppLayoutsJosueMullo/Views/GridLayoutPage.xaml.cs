96585644deeb85477dbfdbdfc9d7e066cf471acd
Josue Mullo <josue.mullo@udla.edu.ec>
2025-05-19 08:50:23 -0500

    Creando la pagina Grid con el layout Grid

---
+++ MauiAppLayoutsJosueMullo/Views/GridLayoutPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿namespace MauiAppLayoutsJosueMullo
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void btnIrAStack_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+}
